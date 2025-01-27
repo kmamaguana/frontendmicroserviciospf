b2097504c68e0467951d6e72d2f9e5f0c8ccd1fa
kmamaguana <kmamaguana@uce.edu.ec>
2025-01-26 21:19:48 -0500

    feat: 🧑‍💻 add navbar with links and search bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,7 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-content">
+        {/* Logo y nombre de la empresa */}
         <Link to="/" className="logo-container">
           <img src="logoc.jpg" alt="Logo" className="logo" />
           <h1 className="company-name">
@@ -13,10 +14,24 @@ const Navbar = () => {
             <span className="highlight-lila-light"> FashionHub</span>
           </h1>
         </Link>
+
+        {/* Menú de navegación y barra de búsqueda alineados a la derecha */}
         <ul className="nav-links">
           <li><Link to="/">Home</Link></li>
           <li><Link to="/products">Products</Link></li>
           <li><Link to="/cart">Cart</Link></li>
+
+          {/* Barra de búsqueda */}
+          <li>
+            <input
+              type="text"
+              placeholder="Search for products..."
+              className="search-bar"
+            />
+          </li>
+
+          {/* Login */}
+          <li><Link to="/login">Login</Link></li>
         </ul>
       </div>
     </nav>
