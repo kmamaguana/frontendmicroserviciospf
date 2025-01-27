cea95facf7b3f3d0ee62178d2bd4b78e855aacdd
kmamaguana <kmamaguana@uce.edu.ec>
2025-01-26 19:24:24 -0500

    feat: 📜 create footer with copyright and responsive design

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2025 My E-Commerce. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
