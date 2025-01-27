984621170f202036fc72e4f6564234bb1c04116e
millanda29 <maikolllanda@gmail.com>
2025-01-27 00:40:38 -0500

    feat: 🛠️ Modify MainContent component to integrate new product and home views

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,18 +1,12 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
-import Products from "../pages/Products";
-import Cart from "../pages/Cart";
+// src/components/Main.js
+import React from 'react';
+import {Outlet} from "react-router-dom";
 import "./MainContent.css";
 
 const MainContent = () => {
   return (
     <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
+      <Outlet/>
     </main>
   );
 };
