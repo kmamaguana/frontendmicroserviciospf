4a86c3fe3b4944ef9c7e49a0e896bfd5f7aa1c32
millanda29 <maikolllanda@gmail.com>
2025-01-28 05:36:41 -0500

    feat: 🚦 Integrate PublicRoute, update routes for better auth handling

--- src/App.js
+++ src/App.js
@@ -3,29 +3,36 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './config/authContext';
 import Layout from "./components/Layout";
 import ProtectedRoute from './config/protectedRoute';
-import Home from './pages/Home';  // Página principal
-import Products from './pages/Products';  // Productos
-import Cart from './pages/Cart';  // Carrito
-import Login from './auth/Login';  // Login
-import Register from './auth/Register';  // Registro
-import ForgotPassword from './auth/ForgotPassword';  // Olvido de contraseña
-import ResetPassword from './auth/ResetPassword';  // Restablecimiento de contraseña
-import AccessDenied from './auth/AccessDenied';  // Página de acceso denegado
+import PublicRoute from './config/publicRoute'; // Nuevo componente para manejar rutas públicas
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Cart from './pages/Cart';
+import Login from './auth/Login';
+import Register from './auth/Register';
+import ForgotPassword from './auth/ForgotPassword';
+import ResetPassword from './auth/ResetPassword';
+import AccessDenied from './auth/AccessDenied';
 import "./App.css";
+import ProductDetail from './pages/ProductDetail';
 
 function App() {
   return (
     <AuthProvider>
       <Router>
         <Routes>
-        <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Layout />}>
-            {/* Rutas públicas */}
-            <Route path="/" element={<Home />} />
-            <Route path="/products" element={<Products />} />
+          {/* Rutas públicas no disponibles para usuarios autenticados */}
+          <Route element={<PublicRoute />}>
+            <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Register />} />
             <Route path="/forgot-password" element={<ForgotPassword />} />
             <Route path="/reset-password" element={<ResetPassword />} />
+          </Route>
+
+          <Route path="/" element={<Layout />}>
+            {/* Rutas públicas accesibles por todos */}
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/products/:id" element={<ProductDetail />} />
           </Route>
 
           {/* Ruta protegida para el carrito (solo usuarios autenticados) */}
@@ -38,7 +45,6 @@ function App() {
           {/* Rutas protegidas para administrador */}
           <Route element={<ProtectedRoute roles={['CUSTOMER']} />}>
             <Route path="/admin" element={<Layout />} />
-            {/* Otras rutas de administración pueden ir aquí */}
           </Route>
 
           {/* Página de acceso denegado */}
