29b4693162fe58e3f63a6b3ccf3113ff9213fbd7
millanda29 <maikolllanda@gmail.com>
2025-01-28 05:35:56 -0500

    refactor: 🛡️ Enhance ProtectedRoute for improved access control

--- src/config/protectedRoute.js
+++ src/config/protectedRoute.js
@@ -1,21 +1,22 @@
 import React, { useContext } from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { AuthContext } from './authContext';
 
 const ProtectedRoute = ({ roles = [] }) => {
-    const { userToken, userRole } = useContext(AuthContext);
+  const { userToken, userRole } = useContext(AuthContext);
+  const location = useLocation();
 
-    if (!userToken) {
-        // Usuarios no autenticado, redirigir a la página de inicio de sesión
-        return <Navigate to="/login" />;
-    }
+  if (!userToken) {
+    // Redirigir al login, pasando la ruta actual en el estado
+    return <Navigate to="/login" state={{ from: location }} />;
+  }
 
-    if (roles.length && !roles.includes(userRole)) {
-        // Usuarios no autorizado, redirigir a una página de acceso denegado
-        return <Navigate to="/access-denied" />;
-    }
+  if (roles.length && !roles.includes(userRole)) {
+    // Redirigir a acceso denegado si el usuario no tiene permisos
+    return <Navigate to="/access-denied" />;
+  }
 
-    return <Outlet />; // Renderiza los componentes hijos cuando el usuario está autenticado y autorizado
+  return <Outlet />; // Renderiza los componentes hijos si está autenticado y autorizado
 };
 
 export default ProtectedRoute;
